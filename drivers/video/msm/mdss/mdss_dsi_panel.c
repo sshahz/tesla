a13fbff637dca164a95ee6a8bf1202dd6c9b7e92
Paul Reioux <reioux@gmail.com>
2016-09-16 20:25:49 +0530

    Add PowerSuspend Driver
    Changed Files:
    - drivers/video/msm/mdss/mdss_dsi_panel.c
    - include/linux/powersuspend.h
    - kernel/power/Kconfig
    - kernel/power/Makefile
    - kernel/power/powersuspend.c
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -30,6 +30,10 @@
 #endif
 #include "mdss_livedisplay.h"
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define DT_CMD_HDR 6
 #define MIN_REFRESH_RATE 48
 #define DEFAULT_MDP_TRANSFER_TIME 14000
@@ -870,6 +874,12 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+	display_on = true;
+
+	#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+	#endif
+
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1007,6 +1017,10 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	mdss_dsi_panel_off_hdmi(ctrl, pinfo);
 
 	display_on = false;
+
+	#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+	#endif
 
 end:
 	/* clear idle state */
