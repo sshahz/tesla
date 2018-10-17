560da733640a480fb1bef4c99231efd4f8bc072c
sf11 <shahzebq14@gmail.com>
2018-10-17 13:23:58 +0000

    Fix Build

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -367,7 +367,6 @@ static int __sprint_symbol(char *buffer, unsigned long address,
 		return sprintf(buffer, "0x%lx", address - symbol_offset);
 
 	if (name != buffer)
-		strcpy(buffer, name);
 	len = strlen(buffer);
 	offset -= symbol_offset;
 
